5dc5cdc75b9b57e90b54b9ba4104a070693cf12a
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-05-05 15:38:13 +1000

    Revert "Merge pull request #1 from cp3402-students/header-modification"
    This reverts commit 076028165b21cbfb2369b02aa0387ac1aa2b7bb6, reversing
    changes made to 6e94bd24a07a89d6cd4794a767009cd246af61b1.

--- jazzy/header.php
+++ jazzy/header.php
@@ -27,7 +27,22 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php the_custom_logo(); ?>
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$jazzy_description = get_bloginfo( 'description', 'display' );
+			if ( $jazzy_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $jazzy_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
