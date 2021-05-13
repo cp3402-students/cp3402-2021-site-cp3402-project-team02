bfe91460b3dfcf1cd0ae9ecd7c70efa58c72a652
aidanmegee <aidan.megee@my.jcu.edu.au>
2021-05-13 13:29:18 +1000

    Modify footer to incorporate colour scheme. Add styling and social links. Add links to footer.php.

--- jazzy/footer.php
+++ jazzy/footer.php
@@ -11,21 +11,21 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jazzy' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jazzy' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jazzy' ), 'jazzy', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+    <div class="social-info">
+        <a href="https://www.facebook.com/Townsville-Jazz-Club-Inc-354976484531491/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="mailto:townsvillejazz@icloud.com"><i class="fas fa-envelope" aria-hidden="true"></i></a>
+        <a href="tel:0413456542"><i class="fas fa-phone" aria-hidden="true"></i></i></a>
+    </div>
+    <div class="site-info">
+        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jazzy' ) ); ?>">
+        </a>
+        <?php
+        /* translators: 1: Theme name, 2: Theme author. */
+        printf( esc_html__( 'Theme: %1$s by %2$s', 'jazzy' ), 'jazzy', '<a href="https://github.com/cp3402-students/cp3402-2021-site-cp3402-project-team02">Group02</a>' );
+        ?>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
